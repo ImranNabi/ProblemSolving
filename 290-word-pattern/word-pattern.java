bd58373428bcb6f791d2ddbd67b7258bfc10b3bf
IMRAN N <165631954+ImranNabi@users.noreply.github.com>
2025-02-24 14:09:38 +0530

    Time: 3 ms (5.80%) | Memory: 41.3 MB (91.49%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -4,16 +4,21 @@ class Solution {
       if(pattern.length()!=words.length) {
         return false;
       } 
-      Set<String>mapSet=new HashSet<>();
-            Set<Character>ch=new HashSet<>();
-                  Set<String>st=new HashSet<>();
-                  for(int i=0;i<pattern.length();i++){
-                    mapSet.add(pattern.charAt(i)+"-"+words[i]);
-                    ch.add(pattern.charAt(i));
-                    st.add(words[i]);
-                  }
-                  return mapSet.size()==ch.size()&& ch.size()==st.size();
-
+      for(int i=0;i<pattern.length();i++){
+        for(int j=i+1;j<pattern.length();j++){
+            if(pattern.charAt(i)==pattern.charAt(j)){
+                if(!words[i].equals(words[j])){
+                    return false;
+                }
+            }
+            else{
+                if(words[i].equals(words[j])){
+                    return false;
+                }
+            }
+        }
+      }
+      return true;
 
     }
 }
